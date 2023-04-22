803b87867640cddba20a7be77105dc6400002fa6
punesh12 <punesh12@gmail.com>
2023-04-22 13:41:12 +0530

    fix/ theme switch smoothening

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -2,6 +2,8 @@ import { createGlobalStyle } from 'styled-components'
 
 import PoppinsBold from '../assets/fonts/Poppins-Bold.ttf'
 import PoppinsRegular from '../assets/fonts/Poppins-Regular.ttf'
+import PoppinsSemiBold from '../assets/fonts/Poppins-SemiBold.ttf'
+import PoppinsMedium from '../assets/fonts/Poppins-Medium.ttf'
 import { colors } from './theme'
 
 export const GlobalStyle = createGlobalStyle`
@@ -10,10 +12,33 @@ export const GlobalStyle = createGlobalStyle`
 @font-face{
     font-family: Regular;
     src:url(${PoppinsRegular}) format('truetype') ;
+    font-display: swap;
+    font-weight: normal;
+    font-style: normal;
+}
+@font-face{
+    font-family: Medium;
+    src:url(${PoppinsMedium}) format('truetype') ;
+    font-display: swap;
+    font-weight: normal;
+    font-style: normal;
+
+}
+@font-face{
+    font-family: SemiBold;
+    src:url(${PoppinsSemiBold}) format('truetype') ;
+    font-display: swap;
+    font-weight: normal;
+    font-style: normal;
+
 }
 @font-face{
     font-family: Bold;
     src:url(${PoppinsBold}) format('truetype') ;
+    font-display: swap;
+    font-weight: normal;
+    font-style: normal;
+
 }
 
 *{
@@ -27,11 +52,6 @@ margin:0;
     color:${colors.blackSecondary};
     font-size: 16px;
     font-weight: 400;
-    font-synthesis: none;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-text-size-adjust: 100%;
     transition: all 0.3s ease-in-out;
 }
 `
